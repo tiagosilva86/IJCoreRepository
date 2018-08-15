3ac01ce471ead2977bacfd4a237474b430b3bd2b
tiagosilva86 <36794945+tiagosilva86@users.noreply.github.com>
2018-08-15 20:15:36 -0300

    commit

---
+++ src/app/Angular/app.angular.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-angular',
+  templateUrl: './app.angular.html',
+  styleUrls: ['./app.angular.css']
+})
+export class AppAngular {
+  title = 'app-cangular';
+}
--- src/app/Angular/app.module.ts
+++ src/app/Angular/app.module.ts
@@ -3,12 +3,11 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-import { AppAngular } from './angular/app.angular';
 
 
 @NgModule({
   declarations: [
-    AppComponent, AppAngular
+    AppComponent
   ],
   imports: [
     BrowserModule
